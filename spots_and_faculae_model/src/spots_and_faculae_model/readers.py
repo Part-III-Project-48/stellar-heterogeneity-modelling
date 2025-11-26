e9bd5075e5886953d10f5335be644e352f359596
BenBlueeeee <benjaminleogreen@gmail.com>
2025-11-26 10:19:56 +0000

    refactor: move read_HARPS_fits to readers.py

--- spots_and_faculae_model/src/spots_and_faculae_model/readers.py
+++ spots_and_faculae_model/src/spots_and_faculae_model/readers.py
@@ -1,7 +1,6 @@
 """file for reading in data from different sources e.g. JWST or HST"""
 
 from pathlib import Path
-from astropy.table import QTable
 import astropy.units as u
 from astropy.io import fits
 from astropy.units import Unit
@@ -69,3 +68,36 @@ def read_JWST_fits_all_spectra(fits_absolute_path : Path, verbose : bool = False
 			spectra.append(spec)
 			
 	return spectra
+
+
+HARPS_WAVELENGTH_UNITS : Unit = u.Angstrom
+HARPS_FLUX_UNITS : Unit = u.MJy
+
+def read_HARPS_fits(fits_absolute_path : Path, verbose : bool = False, name : str = None, INTEGRATION_INDEX : int = 0) -> spectrum:
+	"""
+	Attributes
+	----------
+	verbose : bool (default False)
+		prints a summary of all the headers found in the fits file, as well as the string representation of the header with header index HDU_INDEX
+	"""
+	if not fits_absolute_path.exists():
+		raise FileNotFoundError(f"JWST spectrum file not found at : {fits_absolute_path}.")
+
+	with fits.open(fits_absolute_path) as hdul:
+		HDU_INDEX = 1 # aka EXTRACT1D
+		
+
+		data = hdul[HDU_INDEX].data
+		hdr = hdul[HDU_INDEX].header
+		
+		if verbose:
+			hdul.info()
+			print(repr(hdr))
+
+		# these column name strings are unique to JWST 1D 
+		spec : spectrum = spectrum(wavelengths = data["WAVE"][INTEGRATION_INDEX] * HARPS_WAVELENGTH_UNITS,
+				  fluxes = data["FLUX"][INTEGRATION_INDEX] * HARPS_FLUX_UNITS, name=name, normalised_point=5000*u.Angstrom, normalise_flux=True)
+		
+	return spec
+
+			
